d3532963e1fb476fc08c5f42f90bdb1803d0fac3
Artur Novikau <111690337+ArturNovikov@users.noreply.github.com>
2023-05-30 17:55:59 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,59 @@
-console.log("Цель достигнута!");
+let showMore = document.querySelector(".btn__show-more");
+let sectionElementHide = document.querySelectorAll(
+  ".section-services__element--hide"
+);
+
+let sectionElementHide768 = document.querySelectorAll(
+  ".section-services__element--hide768"
+);
+
+/*показать скрыть выпадающие элементы*/
+showMore.addEventListener("click", function () {
+  let showMoreBtn = document.querySelector(".btn");
+
+  for (let i = 0; i < sectionElementHide768.length; i++) {
+    if (
+      sectionElementHide768[i].classList.contains(
+        "section-services__element--hide768"
+      )
+    ) {
+      sectionElementHide768[i].classList.remove(
+        "section-services__element--hide768"
+      );
+    } else {
+      sectionElementHide768[i].classList.add(
+        "section-services__element--hide768"
+      );
+    }
+  }
+
+  for (let i = 0; i < sectionElementHide.length; i++) {
+    if (
+      sectionElementHide[i].classList.contains(
+        "section-services__element--hide"
+      )
+    ) {
+      sectionElementHide[i].classList.remove("section-services__element--hide");
+    } else {
+      sectionElementHide[i].classList.add("section-services__element--hide");
+    }
+  }
+
+  /* алгоритv замены надписи и поворота стрелок */
+  if (!showMoreBtn.classList.contains("open", "btn--rotate")) {
+    showMoreBtn.classList.add("open", "btn--rotate");
+    showMoreBtn.textContent = "Скрыть";
+  } else {
+    showMoreBtn.classList.remove("open", "btn--rotate");
+    showMoreBtn.textContent = "Показать все";
+  }
+});
+
+/* let createServicesElement = newItemTemplate.cloneNode(true); */
+
+/* поиск шаблона и создание клона */
+/* let sectionTemplate = document.querySelector("#section-template").content;
+let clonedTemplate = sectionTemplate.cloneNode(true);
+let sectionMenu = document.querySelector(".section-services__menu");
+sectionMenu.appendChild(clonedTemplate);
+ */
